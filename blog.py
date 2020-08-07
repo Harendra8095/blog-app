7f2cf9f963375912408458de19fd88d4da433469
Harendra8095 <harendrarathor444@gmail.com>
2020-08-07 17:56:46 +0530

    Testing completed

--- blog.py
+++ blog.py
@@ -32,9 +32,9 @@ def create():
             flash(error)
         else:
             db = get_db()
-            db = execute(
-                'INSERT INTO post (title, body, author_id'
-                'VALUES (?, ?, ?)',
+            db.execute(
+                'INSERT INTO post (title, body, author_id)\
+                VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
             )
             db.commit()
@@ -43,13 +43,13 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        'FROM post p JOIN user u ON p.author_id = u.id'
-        'WHERE p.id = ?',
+        'SELECT p.id, title, body, created, author_id, username\
+        FROM post p JOIN user u ON p.author_id = u.id\
+        WHERE p.id = ?',
         (id,)
     ).fetchone()
 
-    if post in None:
+    if post is None:
         abort(404, "Post id {0} doesn't exist.".format(id))
     if check_author and post['author_id'] != g.user['id']:
         abort(403)
@@ -74,12 +74,12 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                'WHERE id = ?',
+                'UPDATE post SET title = ?, body = ?\
+                WHERE id = ?',
                 (title, body, id)
             )
             db.commit()
-            return redirect(url_for('blox.index'))
+            return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
 
@@ -89,5 +89,5 @@ def delete(id):
     get_post(id)
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
-    db.commmit()
+    db.commit()
     return redirect(url_for('blog.index'))
