d333258c076ab707c2090dfb0e4ca41b261c35db
Harendra8095 <harendrarathor444@gmail.com>
2020-09-25 18:22:55 +0530

    favicon added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -20,6 +20,11 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World again!'
 
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico", mimetype='image/vnd.microsoft.com')
+
+
     from . import db
     db.init_app(app)
 
